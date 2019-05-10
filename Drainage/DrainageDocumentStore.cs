48479139405340a57b11efa2e78563338f6c3bf3
CEDARBARN\JonA <jon.abbott@jppuk.net>
2019-05-10 07:40:15 +0100

    Initial drainage project creation

---
+++ Drainage/DrainageDocumentStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Jpp.Ironstone.Core.Autocad;
+
+namespace Jpp.Ironstone.Drainage.ObjectModel
+{
+    public class DrainageDocumentStore : DocumentStore
+    {
+        public DrainageDocumentStore(Document doc, Type[] managerTypes) : base(doc, managerTypes) { }
+    }
+}
---
+++ Drainage/Managers/DrainageRoutesManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Jpp.Ironstone.Core.Autocad;
+
+namespace Jpp.Ironstone.Drainage.ObjectModel.Managers
+{
+    [Serializable]
+    public class DrainageRoutesManager : AbstractDrawingObjectManager
+    {
+        public override void UpdateDirty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AllDirty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ActivateObjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
