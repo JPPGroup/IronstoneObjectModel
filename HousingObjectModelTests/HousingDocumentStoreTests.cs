b597ab50cb6041c8dc0c32e7dea118956ff3ebc0
jona <jon.abbott@jppuk.net>
2019-10-15 08:11:23 +0100

    Initial object model, test project setup and package update

---
+++ HousingObjectModelTests/HousingDocumentStoreTests.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Jpp.Ironstone.Core.ServiceInterfaces;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Jpp.Ironstone.Housing.ObjectModel.Tests
+{
+    [TestFixture]
+    public class HousingDocumentStoreTests : IronstoneTestFixture
+    {
+        public HousingDocumentStoreTests() : base(Assembly.GetExecutingAssembly(), typeof(HousingDocumentStoreTests)) { }
+
+        [Test]
+        public void VerifyStoreLoaded()
+        {
+            var resultLoaded = RunTest<bool>(nameof(VerifyStoreLoadedResident));
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(resultLoaded, "Data store should be loaded.");
+            });
+        }
+
+        public static bool VerifyStoreLoadedResident()
+        {
+            var acDoc = Application.DocumentManager.MdiActiveDocument;
+            
+            var ds = DataService.Current;
+            ds.InvalidateStoreTypes();
+
+            var store = ds.GetStore<HousingDocumentStore>(acDoc.Name);
+
+            return store != null;
+        }
+    }
+}
