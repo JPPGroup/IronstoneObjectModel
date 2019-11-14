1382216933aacec8c3e02a6adfc38fd1964a0998
jona <jon.abbott@jppuk.net>
2019-11-14 10:20:00 +0000

    Use layer attribute

--- Structures/Constants.cs
+++ Structures/Constants.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Jpp.Ironstone.Core.Autocad;
-
-namespace Jpp.Ironstone.Structures.ObjectModel
+﻿namespace Jpp.Ironstone.Structures.ObjectModel
 {
     class Constants
     {
-        public static LayerInfo EXISTING_TREE_LAYER =
-            new LayerInfo() {LayerId = "JPP_Foundations_TreeRings_Existing", Linetype = "1MM-HIDDEN"};
+        public const string EXISTING_TREE_LAYER = "JPP_Foundations_TreeRings_Existing";
+        public const string PROPOSED_TREE_LAYER = "JPP_Foundations_TreeRings_Proposed";
+        public const string PILED_LAYER = "JPP_Foundations_Pilled";
+        public const string HEAVE_LAYER = "JPP_Foundations_TreeRings_Proposed";
 
-        public static LayerInfo PROPOSED_TREE_LAYER =
-            new LayerInfo() { LayerId = "JPP_Foundations_TreeRings_Proposed"};
-
-        public static LayerInfo PILED_LAYER =
-            new LayerInfo() { LayerId = "JPP_Foundations_Pilled", IndexColor = 10};
-
-        public static LayerInfo HEAVE_LAYER =
-            new LayerInfo() { LayerId = "JPP_Foundations_TreeRings_Proposed", IndexColor = 200};
+        /*
+         * Need to ensure layer properties are in settings file.
+         *
+         * public static LayerInfo EXISTING_TREE_LAYER = new LayerInfo() {LayerId = "JPP_Foundations_TreeRings_Existing", Linetype = "1MM-HIDDEN"};
+         * public static LayerInfo PROPOSED_TREE_LAYER = new LayerInfo() { LayerId = "JPP_Foundations_TreeRings_Proposed"};
+         * public static LayerInfo PILED_LAYER = new LayerInfo() { LayerId = "JPP_Foundations_Pilled", IndexColor = 10};
+         * public static LayerInfo HEAVE_LAYER = new LayerInfo() { LayerId = "JPP_Foundations_TreeRings_Proposed", IndexColor = 200};
+         *
+         */
     }
 }
