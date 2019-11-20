8ada783c1bd25311393fceab987abcbceb972063
jona <jon.abbott@jppuk.net>
2019-11-20 10:08:07 +0000

    Block review layers added to store

---
+++ Housing/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Jpp.Ironstone.Housing.ObjectModel
+{
+    public static class Constants
+    {
+        public const string FOR_REVIEW_GRADIENT_LAYER = "JPP_ForReview_GradientBlocks";
+        public const string FOR_REVIEW_LEVEL_LAYER = "JPP_ForReview_LevelBlocks";
+    }
+}
