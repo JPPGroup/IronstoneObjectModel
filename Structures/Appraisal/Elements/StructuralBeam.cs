ce39491d00244b7463577e9758624277e22209ae
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-04-23 15:10:25 +0100

    WIP

---
+++ Structures/Appraisal/Elements/StructuralBeam.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Jpp.DesignCalculations.Calculations;
+using Jpp.Ironstone.Core.Autocad;
+
+namespace Jpp.Ironstone.Structures.ObjectModel.Appraisal.Elements
+{
+    public class StructuralBeam : StructuralSupportLine, IAppraisalObject
+    {
+        public static StructuralBeam Create(Database database, Point2d start, Point2d end)
+        {
+            Transaction trans = database.TransactionManager.TopTransaction;
+            StructuralBeam newBeam = new StructuralBeam();
+
+            Polyline acPoly = new Polyline();
+            acPoly.SetDatabaseDefaults();
+            acPoly.AddVertexAt(0, start, 0, 0, 0);
+            acPoly.AddVertexAt(1, end, 0, 0, 0);
+
+            newBeam.BaseObject = database.GetModelSpace(true).AppendEntity(acPoly);
+            trans.AddNewlyCreatedDBObject(acPoly, true);
+
+            return newBeam;
+        }
+
+        public Calculation Calculation { get; }
+    }
+}
---
+++ Structures/Appraisal/Elements/StructuralSupportLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using Jpp.Ironstone.Core.Autocad.DrawingObjects.Primitives;
+
+namespace Jpp.Ironstone.Structures.ObjectModel.Appraisal.Elements
+{
+    public abstract class StructuralSupportLine : PolylineDrawingObject
+    {
+        protected override void ObjectModified(object sender, EventArgs e)
+        {
+        }
+
+        protected override void ObjectErased(object sender, ObjectErasedEventArgs e)
+        {
+        }
+    }
+}
