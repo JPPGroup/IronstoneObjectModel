a2b6907dd06af96573d616f8db84d0467e1b40b7
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-04-23 15:10:25 +0100

    WIP

---
+++ Structures/Appraisal/IAppraisalObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jpp.DesignCalculations.Calculations;
+
+namespace Jpp.Ironstone.Structures.ObjectModel.Appraisal
+{
+    interface IAppraisalObject
+    {
+        Calculation Calculation { get; }
+    }
+}
---
+++ Structures/Appraisal/StructuralElementManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+using Autodesk.AutoCAD.ApplicationServices;
+using DesignCalculations.Engine;
+using Jpp.Ironstone.Core.Autocad;
+using Jpp.Ironstone.Core.ServiceInterfaces;
+
+namespace Jpp.Ironstone.Structures.ObjectModel.Appraisal
+{
+    [Layer(Name = "JPP_Structures_BeamCentreline")]
+    [Layer(Name = "JPP_Structures_LoadbearingWall")]
+    public class StructuralElementManager : AbstractDrawingObjectManager<DrawingObject>  
+    {
+        [XmlIgnore] 
+        private CalculationSet _calculations;
+
+        public StructuralElementManager(Document document, ILogger log) : base(document, log)
+        {
+        }
+
+        public StructuralElementManager() : base()
+        {
+        }
+
+        public override void Add(DrawingObject toBeManaged)
+        {
+            if (toBeManaged is IAppraisalObject ao)
+            {
+                _calculations.AddCalculation(ao.Calculation);
+                base.Add(toBeManaged);
+            }
+            else
+            {
+                throw new InvalidOperationException("Object must be an appraisal object");
+            }
+        }
+    }
+}
