acd6d4e1c39a410e3ae6b0cf60c6962bd5d71409
MichaelLiddiard <michael.liddiard@jppuk.net>
2023-01-24 11:03:14 +0000

    Correction for failing test due to layer names

--- HousingTests/HousingDocumentStoreTests.cs
+++ HousingTests/HousingDocumentStoreTests.cs
@@ -48,19 +48,21 @@ namespace Jpp.Ironstone.Housing.ObjectModel.Tests
         {
             var acDoc = Application.DocumentManager.MdiActiveDocument;
             var acDb = acDoc.Database;
+            string layername;
 
             using (var trans = acDb.TransactionManager.StartTransaction())
             {
                 var ds = DataService.Current;
                 ds.InvalidateStoreTypes();
                 var unused = ds.GetStore<HousingDocumentStore>(acDoc.Name);
+                layername = unused.LayerManager.GetLayerName(Constants.FOR_REVIEW_GRADIENT_LAYER);
 
                 trans.Commit();
             }
 
             using (var unused = acDb.TransactionManager.StartTransaction())
             {
-                return acDb.GetLayer(Constants.FOR_REVIEW_GRADIENT_LAYER) != null;
+                return acDb.GetLayer(layername) != null;
             }
         }
         
@@ -68,19 +70,21 @@ namespace Jpp.Ironstone.Housing.ObjectModel.Tests
         {
             var acDoc = Application.DocumentManager.MdiActiveDocument;
             var acDb = acDoc.Database;
+            string layername;
 
             using (var trans = acDb.TransactionManager.StartTransaction())
             {
                 var ds = DataService.Current;
                 ds.InvalidateStoreTypes();
                 var unused = ds.GetStore<HousingDocumentStore>(acDoc.Name);
+                layername = unused.LayerManager.GetLayerName(Constants.FOR_REVIEW_LEVEL_LAYER);
 
                 trans.Commit();
             }
 
             using (var unused = acDb.TransactionManager.StartTransaction())
             {
-                return acDb.GetLayer(Constants.FOR_REVIEW_LEVEL_LAYER) != null;
+                return acDb.GetLayer(layername) != null;
             }
         }
     }
